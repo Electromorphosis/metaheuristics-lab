0834b2428313703f97e72e728508ec9941c61137
mechlinski.pawel <mechlinski.pawel@gmail.com>
2024-06-16 17:21:34 +0200

    generate random, neigh solutions, compare and print pretty

--- graphs.cpp
+++ graphs.cpp
@@ -4,6 +4,9 @@
 #include "include/namespace.h"
 #include "include/Exporter.h"
 using namespace mhe;
+using std::cout;
+using std::cerr;
+using std::vector;
 
 bool parseArguments(int argc, char* argv[], int& size, int& minWeight, int& maxWeight) {
     for (int i = 1; i < argc; ++i) {
@@ -24,16 +27,38 @@ bool parseArguments(int argc, char* argv[], int& size, int& minWeight, int& maxW
 int main(int argc, char* argv[]) {
 
     if (!parseArguments(argc, argv, SIZE, MIN_WEIGHT, MAX_WEIGHT) || SIZE <= 0 || MIN_WEIGHT > MAX_WEIGHT) {
-        std::cerr << "Usage: " << argv[0] << " --size=<size> --min_weight=<min_weight> --max_weight=<max_weight>" << std::endl;
-        std::cerr << "Ensure size is positive and min_weight <= max_weight. Use only integers." << std::endl;
+        cerr << "Usage: " << argv[0] << " --size=<size> --min_weight=<min_weight> --max_weight=<max_weight>" << std::endl;
+        cerr << "Ensure size is positive and min_weight <= max_weight. Use only integers." << std::endl;
         return 1;
     }
+    SIZE = 20;
     // Generate random weighed graph in a form of Adjacency Matrix
     srand(static_cast<unsigned int>(time(0))); // Seed the random number generator
-    std::vector<std::vector<int>> adjMatrix = generateRandomGraphAdjacencyMatrix();
+    vector<vector<int>> adjMatrix = generateRandomGraphAdjacencyMatrix();
 
 
-//    // Visualize initial results
-//    printPrettyMatrix(adjMatrix);
+    // Visualize initial results
+    // printPrettyMatrix(adjMatrix);
+    cout << "Graph snippet:\n";
+    printAdjMatrixRow(adjMatrix, 0);
+    printAdjMatrixRow(adjMatrix, 1);
+    cout << "...\n";
 
+
+    int k = 3;
+    cout << "============\nObjective:\n\tK number: " << k << "\n";
+    vector<int> nodeRandomAssignment = randomAssign(adjMatrix, k);
+    cout << "\nRandom assignment\t\t";
+    printAssignment(nodeRandomAssignment);
+    cout << "\nRandom neighbour 1\t\t";
+    vector<int> nodeRandomNeighbour = generateNeighbourSolution(nodeRandomAssignment, k);
+    printAssignment(nodeRandomNeighbour);
+    compareAssignments(nodeRandomAssignment, nodeRandomNeighbour);
+
+    cout << "\n\nRandom neighbour 1\t\t";
+    printAssignment(nodeRandomNeighbour);
+    cout << "\nNeighbour 2nd gen\t\t";
+    vector<int> nodeRandomNeighbour2 = generateNeighbourSolution(nodeRandomNeighbour, k);
+    printAssignment(nodeRandomNeighbour2);
+    compareAssignments(nodeRandomNeighbour, nodeRandomNeighbour2);
 }
--- include/namespace.h
+++ include/namespace.h
@@ -8,22 +8,26 @@
 #include <vector>
 #include <cstdlib>
 #include <ctime>
+#include <random>
 
 namespace mhe {
-
+    using std::cout;
+    using std::vector;
     const int NULL_WEIGHT = 0; // Value representing no edge
     int MIN_WEIGHT = 1; // Minimum weight of the edges
     int MAX_WEIGHT = 9; // Maximum weight of the edges
     int SIZE = 9; // size of the matrix; sizes above 9 might not be fully well presented by visualizers
-
+    std::random_device rd;
+    std::mt19937 mt(rd());
 
 
     int getRandomWeight() {
-        return MIN_WEIGHT + rand() % (MAX_WEIGHT - MIN_WEIGHT + 1);
+        std::uniform_int_distribution<int> distr(MIN_WEIGHT, MAX_WEIGHT);
+        return distr(mt);
     }
 
     std::vector<std::vector<int>> generateRandomGraphAdjacencyMatrix() {
-        std::cout << "Generating graph with parameters:\nSize: " << SIZE << "\nMinWeight: " << MIN_WEIGHT << "\nMaxWeight: " << MAX_WEIGHT << "\n";
+        std::cout << "Generating graph with parameters:\n\tSize: " << SIZE << "\n\tMinWeight: " << MIN_WEIGHT << "\n\tMaxWeight: " << MAX_WEIGHT << "\n";
         std::vector<std::vector<int>> matrix(SIZE, std::vector<int>(SIZE, NULL_WEIGHT));
 
         for (int i = 0; i < SIZE; ++i) {
@@ -36,6 +40,19 @@ namespace mhe {
 
         return matrix;
     }
+
+    std::vector<int> generateNeighbourSolution(const std::vector<int>& inputSolution, int k) {
+        std::vector<int> result = inputSolution;
+        // In a most "minimal" way possible - flip one random vertice to the next (in a sense of modular arithmetic...)
+        // partition
+        int nodesNum = inputSolution.size();
+        std::uniform_int_distribution<int> distrNodeRange(0, nodesNum - 1);
+        int randomNodeNum = distrNodeRange(mt);
+        result.at(randomNodeNum) = (result.at(randomNodeNum) + 1);
+        if (result.at(randomNodeNum) > k) result.at(randomNodeNum) = 0;
+        return result;
+    }
+
 
     // Function to print the adjacency matrix
     void printAdjMatrix(const std::vector<std::vector<int>>& matrix) {
@@ -80,6 +97,61 @@ namespace mhe {
         for (int i = 0; i < SIZE; i++) {
             std::cout << i << "| ";
             printAdjMatrixRow(adjMatrix, i);
+        }
+    }
+
+    std::vector<int> randomAssign(const std::vector<std::vector<int>>&inputGraph, int k) {
+        std::vector<int> nodeAssginment;
+        // This method assigns randomly each node in the graph to one of the partitions (i.e. making random cut).
+        if(k <= 0) {
+            std::cerr << "Provided k value must be greater than 0!";
+            return nodeAssginment;
+        }
+
+        std::uniform_int_distribution<int> distrPartRange(0, k);
+        for (int i = 0; i < inputGraph.size(); i++) {
+            nodeAssginment.push_back(distrPartRange(mt));
+        }
+
+        return nodeAssginment;
+    }
+
+    void printAssignment(const std::vector<int>&assignment, bool dict = false) {
+        cout << "= { ";
+            for (int i = 0; i < assignment.size(); i++) {
+                if(assignment.size() != i+1) {
+                    if (dict) {
+                        cout << i << ":" << assignment.at(i) << ", ";
+                    }
+                    cout << assignment.at(i) << ", ";
+                } else {
+                    cout << assignment.at(i);
+                }
+            }
+            cout << " }";
+    }
+
+    void compareAssignments(vector<int> firstAssignment, vector<int> secondAssignment) {
+        if (firstAssignment.size() != secondAssignment.size()) {
+            cout << "\nAssignments are not the same size! First one is " << firstAssignment.size() << ", second one is " << secondAssignment.size() << ".";
+            return;
+        }
+        int sumOfDifferences = 0;
+        int proximity = 0;
+
+        for (int i = 0; i < firstAssignment.size(); i++) {
+            if(firstAssignment.at(i) != secondAssignment.at(i)) {
+                cout << "\nDifference occur in the affiliation of the node number " << i << " (" << firstAssignment.at(i) << ", " << secondAssignment.at(i) << " )";
+                sumOfDifferences++;
+                proximity += abs(firstAssignment.at(i)-secondAssignment.at(i));
+            }
+        }
+
+        if (sumOfDifferences == 0) {
+            cout << "\nBoth assignments are the same assignment!";
+        } else {
+            cout << "\nThere are " << sumOfDifferences << " difference(s) between both assignments.";
+            cout << "\nThe value of their Proximity score is: " << proximity;
         }
     }
 }
