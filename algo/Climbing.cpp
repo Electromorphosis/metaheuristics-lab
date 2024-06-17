09ef2b2bce05c2df6567631bc429da5ef3274cc6
mechlinski.pawel <mechlinski.pawel@gmail.com>
2024-06-17 21:40:39 +0200

    Climbing algo implementation - todo is the presentation of results in graph.cpp (need also to start testing all algos in the same, relatively big example graph)

---
+++ algo/Climbing.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mechlon on 17.06.24.
+//
+
+#include "Climbing.h"
---
+++ algo/Climbing.h
@@ -0,0 +1,54 @@
+//
+// Created by mechlon on 17.06.24.
+//
+
+#ifndef MHE_CLIMBING_H
+#define MHE_CLIMBING_H
+#include <string>
+#include <vector>
+#include "Algo.h"
+
+class Climbing : public Algo {
+public:
+    static std::vector<int> calculate(const std::vector<std::vector<int>>& graph, const int k) {
+        std::vector<int> check = checkIfValidInput(graph, k);
+        if (check.at(0) == -1) {
+            return check;
+        }
+
+        // TODO: Might want check other strategies for first assignment ?
+        std::vector<int> currentAssignment(randomAssign(graph, k));
+
+        int minCost = getAssignmentValue(graph, currentAssignment);
+        bool improvementFound = true;
+
+        while (improvementFound) {
+            improvementFound = false;
+
+            // Explore all neighbors and find the one with the minimal cost
+            for (int i = 0; i < graph.size(); ++i) {
+                for (int j = 0; j < k; ++j) {
+                    if (currentAssignment[i] != j) {
+                        // Create a copy of the current assignment
+                        std::vector<int> neighbor(currentAssignment);
+                        neighbor[i] = j;
+
+                        int neighborCost = getAssignmentValue(graph, neighbor);
+                        if (neighborCost < minCost) {
+                            currentAssignment = neighbor;
+                            minCost = neighborCost;
+                            improvementFound = true;
+                            break; // Early termination if better neighbor found
+                         }
+                    }
+                }
+            }
+        }
+
+        return currentAssignment;
+    }
+
+};
+
+
+#endif //MHE_CLIMBING_H
